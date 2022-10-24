731ecc08e7e333275c2b3804713d8bcad5d5b6c3
APAC\CIO2SZH <Lingyang.Cao@cn.bosch.com>
2022-10-24 17:09:39 +0800

    -光效及材质添加 -视觉差

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -4,59 +4,99 @@ using UnityEngine;
 
 public class CharacterController2D : MonoBehaviour
 {
-    // 
     private Rigidbody2D _rigidbody2D;
     private Animator _animator;
+    private Renderer _render;
     public AudioSource _jumpAudio;
-    public Renderer _render;
 
-    /** MoveConfig **/
-    [HideInInspector][SerializeField] private float _running; // record moving status
-    [HideInInspector][SerializeField] private float _movementSpeed = 6f; // moving speed
+    // Move Config
+    [HideInInspector][SerializeField] private float running; // 记录移动的状态
+    //
+    [HideInInspector][SerializeField] private float _movementSpeed = 6f; // 移动速度
     [HideInInspector][Range(0, 0.3f)][SerializeField] private float _movementSmoothing = 0.05f; // How much to smooth out the movement
 
     // Jump Info
-    [HideInInspector][Range(0, 8f)][SerializeField] private float _jumpForce = 8f; // Amount of force added when the player jumps.
+    private bool jumpPressed = false; // 是否按下跳跃键
+    [HideInInspector][SerializeField] private int jumpCount; // 可跳跃次数记录
+    private bool isLanded;
+    //
+    [HideInInspector][Range(0, 8f)][SerializeField] private float _jumpForce = 8f; // 跳跃力度
+    [HideInInspector][SerializeField] private int _jumpAllowCount = 2; // 最大跳跃次数
     [HideInInspector][SerializeField] private bool _canAirControl = false; // 在空中是否可以转向
-    [HideInInspector][SerializeField] private int _jumpDefaultCount = 2; // Whether or not a player can steer while jumping;
-    [HideInInspector][SerializeField] private int _jumpCount; //
-    private bool _isLanded; // Whether or not a player can steer while jumping;
-    // Ground Info
-    [SerializeField] private bool _isGrounded = false;
-    [HideInInspector][SerializeField] private LayerMask _ground; // 用于判断地面的图层
-    [HideInInspector][SerializeField] private Transform _groundCheck; // 地面判断点
-    const float _groundCheckRadius = .2f; // 地面判断半径
+    [HideInInspector][SerializeField] private float fallGravityMultiplier = 2; // 下落加速度倍数
+    //
+    [SerializeField] private bool isGrounded = false;
+    //
+    [HideInInspector] public LayerMask _ground; // 用于判断为地面的图层
+    [HideInInspector][SerializeField] public Transform _groundCheck; // 脚底判断接触的检查点
+    const float _groundCheckRadius = .2f; // 脚底判断半径
 
     // Crouch Info
-    private bool _isCrouch, _canStand; // Whether or not the player is crouched
+    private bool isCrouch, _canStand; // Whether or not the player is crouched
+    //
     [HideInInspector][Range(0, 1)][SerializeField] private float _crouchSpeed = 0.36f; // 下蹲的移动速度
     // Ceiling Info
-    [HideInInspector][SerializeField] private Collider2D _colliderCeilingDisable; // 头部碰撞体，下蹲时失效
-    [HideInInspector][SerializeField] public Transform _ceilingCheck; // 头顶判断点
+    [HideInInspector][SerializeField] public Collider2D _colliderCeilingDisable; // 头部碰撞体，下蹲时失效
+    [HideInInspector][SerializeField] public Transform _ceilingCheck; // 头顶判断接触的检查点
     const float _ceilingCheckRadius = .2f; // 头顶判断半径
 
     private Vector3 velocity = Vector3.zero;
     private bool _facingRight = true;  // For determining which way the player is currently facing.
 
-    private bool _isHurt; // 角色受伤状态
+    private bool isHurt; // 角色受伤状态
+
     public float blinkTime;
     public int blinks;
     [HideInInspector][SerializeField] private float _hurtForce = 2f;
+
+    // 按键监测
+    private float x, y;
+    private float xRaw, yRaw;
+    // 角色状态
+    private bool crouchPressed = false;
 
     void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
         _render = GetComponent<Renderer>();
+    }
 
+    void Start()
+    {
+        // [Jump]初始化跳跃次数
+        jumpCount = _jumpAllowCount;
+    }
+
+    void Update()
+    {
+        // 获取按键
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        xRaw = Input.GetAxisRaw("Horizontal");
+        yRaw = Input.GetAxisRaw("Vertical");
+        // 长按C键下蹲，松开恢复
+        if (Input.GetButtonDown("Crouch"))
+            crouchPressed = true;
+        else if (Input.GetButtonUp("Crouch"))
+            crouchPressed = false;
+        // [Jump]单击空格键跳跃
+        if (Input.GetButtonDown("Jump"))
+            jumpPressed = true;
     }
 
     void FixedUpdate()
     {
         // 重置落地状态
-        _isLanded = false;
+        isLanded = false;
         // 地面检测
         CheckLanded();
+        //
+        Move(xRaw, crouchPressed);
+        // [Jump]
+        Jump();
+        // [Jump]下落加速
+        if (_rigidbody2D.velocity.y < 0) _rigidbody2D.velocity += Vector2.up * Physics2D.gravity.y * (fallGravityMultiplier - 1) * Time.fixedDeltaTime;
     }
 
     /// <summary>
@@ -65,13 +105,12 @@ public class CharacterController2D : MonoBehaviour
     /// <param name="move">左右移动方向</param>
     /// <param name="crouch">是否下蹲</param>
     /// <param name="jump">是否跳跃</param>
-    public void Move(float move, bool crouch, bool jump)
+    public void Move(float move, bool crouch)
     {
         // 没有受伤时才能操作
-        if (!_isHurt)
+        if (!isHurt)
         {
             Movement(move, crouch);
-            Jump(jump);
         }
         //
         SwitchAnimation();
@@ -96,9 +135,9 @@ public class CharacterController2D : MonoBehaviour
                 crouch = true;
             }
         }
-        _isCrouch = crouch;
+        isCrouch = crouch;
         // 角色在地面上
-        if (_isGrounded)
+        if (isGrounded)
         {
             // If crouching
             if (crouch)
@@ -114,14 +153,14 @@ public class CharacterController2D : MonoBehaviour
                 if (_colliderCeilingDisable != null) _colliderCeilingDisable.enabled = true;
             }
             //
-            _running = move;
+            running = move;
             // Move the character by finding the target velocity
             Vector3 targetVelocity = new Vector2(move * _movementSpeed, _rigidbody2D.velocity.y);
             // And then smoothing it out and applying it to the character
             _rigidbody2D.velocity = Vector3.SmoothDamp(_rigidbody2D.velocity, targetVelocity, ref velocity, _movementSmoothing);
         }
         // 角色在地面上 或 空中允许 才可转向
-        if (_isGrounded || _canAirControl)
+        if (isGrounded || _canAirControl)
         {
             // If the input is moving the player right and the player is facing left...
             if (move > 0 && !_facingRight)
@@ -153,46 +192,43 @@ public class CharacterController2D : MonoBehaviour
     /// <summary>
     /// 跳跃脚本
     /// </summary>
-    /// <param name="jump"></param>
-    private void Jump(bool jump)
+    private void Jump()
     {
         // 按键触发了跳跃
-        if (jump)
+        if (jumpPressed)
         {
+            jumpPressed = false;
             // 当前剩余跳跃次数
-            if (_jumpCount > 0)
+            if (jumpCount < 1) return;
+            else
             {
                 // 跳跃受力
                 _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _jumpForce);
                 // 减少次数
-                _jumpCount--;
+                jumpCount--;
                 // 清除降落状态
-                if (_isLanded) _isLanded = false;
+                if (isLanded) isLanded = false;
             }
         }
     }
-
-    /// <summary>
-    /// 地面检测
-    /// </summary>
     private void CheckLanded()
     {
         // 初始状态
-        bool wasGrounded = _isGrounded;
+        bool wasGrounded = isGrounded;
         // 
-        _isGrounded = Physics2D.OverlapCircle(_groundCheck.position, _groundCheckRadius, _ground);
+        isGrounded = Physics2D.OverlapCircle(_groundCheck.position, _groundCheckRadius, _ground);
         // 落地之后
-        if (_isGrounded && !wasGrounded)
+        if (isGrounded && !wasGrounded)
         {
-            _isLanded = true;
+            isLanded = true;
             // 受伤后着地则结束受伤
-            if (_isHurt) { _isHurt = false; }
+            if (isHurt) { isHurt = false; }
             // 重置跳跃次数
-            _jumpCount = _jumpDefaultCount;
+            jumpCount = _jumpAllowCount;
         }
         //
         //Debug.Log(Mathf.Abs(_rigidbody2D.velocity.x));
-        //if (_isHurt && Mathf.Abs(_rigidbody2D.velocity.x) < 0.1f) { _isHurt = false; }
+        //if (isHurt && Mathf.Abs(_rigidbody2D.velocity.x) < 0.1f) { isHurt = false; }
         //
         //GameObject[] targetObjects = System.Array.ConvertAll(Physics.OverlapCircleAll(_groundCheck.position, _groundCheckRadius, _ground, collider => collider.gameObject);
     }
@@ -207,23 +243,27 @@ public class CharacterController2D : MonoBehaviour
             if (_animator.GetBool("isFall"))
             {
                 enemy.Jumpon();
+                // 消灭敌人后跳起
+                _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _jumpForce / 2);
             }
             // 受伤后移动
             else
             {
+                // 受伤的受力
                 if (transform.position.x < other.gameObject.transform.position.x)
                 {
-                    _isHurt = true;
+                    isHurt = true;
                     _rigidbody2D.velocity = new Vector2(-1 * _hurtForce, _hurtForce);
                 }
                 else if (transform.position.x > other.gameObject.transform.position.x)
                 {
-                    _isHurt = true;
+                    isHurt = true;
                     _rigidbody2D.velocity = new Vector2(_hurtForce, _hurtForce);
                 }
                 //
-                if (_isHurt)
+                if (isHurt)
                 {
+                    // 受伤效果
                     GameController.Instance.UpdateHealth(-1);
                     GameController.Instance.RefreshGameInfos();
                     //
@@ -249,24 +289,26 @@ public class CharacterController2D : MonoBehaviour
     void SwitchAnimation()
     {
         // 奔跑
-        _animator.SetFloat("running", Mathf.Abs(_running));
+        _animator.SetFloat("running", Mathf.Abs(running));
         // 下蹲
-        _animator.SetBool("isCrouch", _isCrouch);
+        _animator.SetBool("isCrouch", isCrouch);
         // 跳跃|降落
-        _animator.SetBool("isJump", !_isGrounded);
+        _animator.SetBool("isJump", !isGrounded);
         _animator.SetBool("isFall", false);
-        if (!_isGrounded && _rigidbody2D.velocity.y < 0)
+        if (!isGrounded && _rigidbody2D.velocity.y < 0)
         {
             _animator.SetBool("isJump", false);
             _animator.SetBool("isFall", true);
         }
         // 重返地面
-        if (_isLanded)
+        if (isLanded)
         {
             _animator.SetTrigger("triLanded");
         }
+        //
+        if (isHurt && Mathf.Abs(_rigidbody2D.velocity.x) < 0.1f) { isHurt = false; }
         // 受伤
-        _animator.SetBool("isHurt", _isHurt);
+        _animator.SetBool("isHurt", isHurt);
     }
 
     /// <summary>
@@ -276,7 +318,7 @@ public class CharacterController2D : MonoBehaviour
     {
         // 地面检测
         Gizmos.color = Color.green;
-        if (_isGrounded) { Gizmos.color = Color.red; }
+        if (isGrounded) { Gizmos.color = Color.red; }
         Gizmos.DrawWireSphere(_groundCheck.position, _groundCheckRadius);
         // 头顶检测
         Gizmos.color = Color.red;
--- Assets/Scripts/Character_Player.cs
+++ Assets/Scripts/Character_Player.cs
@@ -7,17 +7,8 @@ public class Character_Player : MonoBehaviour
     // 角色通用脚本
     private CharacterController2D _chaControl;
 
-    // 角色状态
-    private bool _isJump = false;
-    private bool _isCrouch = false;
-
     // 死亡边界
     public float deadLiney = -7.0f;
-
-    // 按键监测
-    private float x, y;
-    private float xRaw, yRaw;
-
 
     // Start is called before the first frame update
     void Start()
@@ -31,28 +22,14 @@ public class Character_Player : MonoBehaviour
         // 角色掉落，重置游戏
         if (transform.position.y < deadLiney)
         {
+            GetComponent<AudioSource>().enabled = false;
             GameController.Instance.Restart();
         }
-        // 获取按键
-        x = Input.GetAxis("Horizontal");
-        y = Input.GetAxis("Vertical");
-        xRaw = Input.GetAxisRaw("Horizontal");
-        yRaw = Input.GetAxisRaw("Vertical");
-        // 单机空格键跳跃
-        if (Input.GetButtonDown("Jump"))
-            _isJump = true;
-        // 长按C键下蹲
-        if (Input.GetButtonDown("Crouch"))
-            _isCrouch = true;
-        else if (Input.GetButtonUp("Crouch"))
-            _isCrouch = false;
+
     }
 
     void FixedUpdate()
     {
-        _chaControl.Move(xRaw, _isCrouch, _isJump);
-        //
-        _isJump = false;
     }
 
 }
